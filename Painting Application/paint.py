2bb0cce2558ee5583fe2d694350cbed7d780b8f3
Matthew Lee <nuguri12@gmail.com>
2024-01-19 13:00:27 +0900

    -paint tutorial #2

--- Painting Application/paint.py
+++ Painting Application/paint.py
@@ -1,11 +1,23 @@
+from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.graphics import Color, Ellipse, Line
 
 class MyPaintWidget(Widget):
-    pass
+    
+        def on_touch_down(self, touch):
+            color = (random(), random(), random())
+            with self.canvas:
+                Color(*color)
+                d = 50.
+                Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size = (d, d))
+                touch.ud['line'] = Line(points = (touch.x, touch.y))
+                
+        def on_touch_move(self, touch):
+            touch.ud['line'].points += [touch.x, touch.y]
 
 class MyPaintApp(App):
+    
     def build(self):
         return MyPaintWidget()
     
